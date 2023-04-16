8c0704515d31f9af2f925d0773a6e98d711be810
Nadir <nadir@MacBook-Pro-Nadir.local>
2023-04-17 00:07:41 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,35 @@
+ require('dotenv').config()
+ const sequelize = require('./db.js')
+ const express = require('express')
+ const models = require('./models/models.js')
+ const cors = require('cors')
+ const fileUpload = require('express-fileupload')
+ const router = require('./routes/index.js')
+ const errorHandler = require('./middleware/ErrorHandlingMiddleware.js')
+ const path = require('path')
+
+ const PORT = process.env.PORT || 5070;
+
+ const app = express()
+ app.use(cors())
+ app.use(express.json())
+ app.use(express.static(path.resolve(__dirname, 'static')))
+ app.use(fileUpload({}))
+ app.use('/api', router)
+
+
+ app.use(errorHandler)
+ 
+
+ const start = async() => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync ()
+        app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+ }
+
+
+ start()
